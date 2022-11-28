4b61e6ec76297f959508b801ef6fa33ba9115774
Stu Last <105198429+Stu-Last@users.noreply.github.com>
2022-11-28 13:26:08 +0000

    Add first step Models and Controllers

---
+++ HPlusSport.API/Controllers/ProductsController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HPlusSport.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+// For more information on enabling Web API for 
+
+namespace HPlusSport.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class ProductsController : ControllerBase
+    {
+        private readonly ShopContext _context;
+
+        public ProductsController(ShopContext context)
+        {
+            _context = context;
+
+            _context.Database.EnsureCreated();
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Product>>> GetAllProducts ()
+        {
+            var products = _context.Products.ToList();
+            return Ok(products); ;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProduct(int id)
+        {
+            var product = await _context.Products.FirstOrDefaultAsync(p => p.Id == id);
+            return Ok(product);
+        }
+    }
+}
+
---
+++ HPlusSport.API/Models/ShopContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace HPlusSport.API.Models
+{
+	public class ShopContext : DbContext
+	{
+		public ShopContext(DbContextOptions<ShopContext> options): base(options)
+		{
+		}
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+			//Define relationship between tables.
+			modelBuilder.Entity<Category>()
+				.HasMany(p => p.Products)
+				.WithOne(c => c.Category)
+				.HasForeignKey(c => c.CategoryId);
+
+			modelBuilder.Seed();
+        }
+
+        public DbSet<Product> Products { get; set; }
+		public DbSet<Category> Categories { get; set; }
+	}
+}
+
