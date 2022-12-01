7b9824a98a0e2313eadea8be7345abb87470a4ec
Stu Last <stuart.last@pendennis.com>
2022-12-01 13:24:25 +0000

    Completed unnecessarily simple C# API backend course on Linked In

--- HPlusSport.API/Models/Product.cs
+++ HPlusSport.API/Models/Product.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 using System.Text.Json.Serialization;
 
 namespace HPlusSport.API.Models
@@ -6,14 +7,18 @@ namespace HPlusSport.API.Models
 	public class Product
 	{
 		public int Id { get; set; }
+		[Required]
 		public string Sku { get; set; } = String.Empty;
+		[Required]
 		public string Name { get; set; } = String.Empty;
+		[Required]
 		public string Description { get; set; } = String.Empty;
-		public decimal Price { get; set; }
-		public bool IsAvailable { get; set; }
+		public decimal Price { get; set; } = 0;
+		public bool IsAvailable { get; set; } = false;
+		[Required]
 		public int CategoryId { get; set; }
 		[JsonIgnore]
-		public virtual Category Category { get; set; }
+		public virtual Category? Category { get; set; }
 	}
 }
 
--- HPlusSport.API/Program.cs
+++ HPlusSport.API/Program.cs
@@ -4,8 +4,11 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+//builder.Services.AddControllers().ConfigureApiBehaviorOptions(options =>
+//{
+//    options.SuppressModelStateInvalidFilter = true;
+//});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
